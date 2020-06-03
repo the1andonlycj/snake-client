95e182a7ee2abab73c34f0b9b27cde232893eef8
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-03 20:29:30 +0000

    Finalized the data forthe three separate modules: input (controls), client (server connection), play (putting them together)

--- input.js
+++ input.js
@@ -1,16 +1,50 @@
-const setupInput = function () {
+const { connect } = require('./client');
+let connection;
+
+
+const setupInput = function (conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', handleUserInput);
+  connection = conn;
   return stdin;
 };
 
+
 const handleUserInput = function(key) {
-  if (key = "\u0003") {
+  if (key === "\u0003") {
     process.exit()}; 
+
+  if (key === 'w') {
+    setTimeout(() => {
+        connection.write("Move: up");
+      }, 100)
+    };
   
+  if (key === 'a') {
+    setTimeout(() => {
+      connection.write("Move: left");
+    }, 100)
+  };
+
+  if (key === 's') {
+    setTimeout(() => {
+      connection.write("Move: down");
+    }, 100)
+    };
+
+  if (key === 'd') {
+    setTimeout(() => {
+      connection.write("Move: right");
+    }, 100)
+    };
+
+
+
+
+
   };
 
 module.exports = { setupInput };
